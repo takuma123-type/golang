faaac26a30326090b0a2e4c4323f22ed3c8e11d6
takuma <seishirou1270@gmail.com>
2022-07-01 14:20:05 +0900

    commit

---
+++ go/cmd/hello/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Stringfy interface {
+	ToString() string
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+func (p *Person) ToString() string {
+	return fmt.Println("Name=%v, Age=%v", p.Name, p.Age)
+}
+
+type Car struct {
+	Number string
+	Model  string
+}
+
+func (c *Car) ToString() string {
+	return fmt.Println("Number=%v, Model=%v", c.Number, c.Model)
+}
+
+
+func main() {
+	vs := []Stringfy{
+		&Person{Name: "Taro", Age: 21},
+		&Car{Number: "123-456", Model: "AB-1234"},
+	}
+
+	for _, v := range vs {
+		fmt.Println(v.ToString())
+	}
+}
